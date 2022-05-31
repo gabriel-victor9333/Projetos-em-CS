c05303a2496d84a0b0b0925a6fed0131c28b6d28
gabriel-victor9333 <gabrielvictor933@gmail.com>
2022-05-30 21:19:41 -0300

    Primeiros passos no C#

---
+++ Program1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o seu nome:");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + name);
+
+            Console.WriteLine("pressione enter para termina");
+
+            Console.Read();
+
+
+
+        }
+    }
+}
